2079b408a6ebf53e4733290d13777d946091a1b4
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 18:51:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,12 @@ class Song
   
   @@count = 0
   @@artists = []
-  @@genre = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @@count += 1
     @@artists << artist
-    @@genre << genre
+    @@genres << genre
     
     @name = name
     @artist = artist
