40917b2f178e1c682c7d34ec2e4c607cc8e49283
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 18:40:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,25 @@
 class Song
   attr_reader :name, :artist, :genre
   
-  @@song_count = 0
+  @@count = 0
   @@artists = []
+  @@genre = []
   
-  def initialize
-    @@song_count += 1
-    @@
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artists << artists
+    @@genre << genre
+    
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres
   end
 end
